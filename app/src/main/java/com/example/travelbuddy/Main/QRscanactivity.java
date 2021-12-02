382848333cad2c84668366f10e52c6bcef39d0c8
LordofDucks123 <freumert@gmail.com>
2021-12-02 14:00:56 +0100

    Camera

--- app/src/main/java/com/example/travelbuddy/Main/QRscanactivity.java
+++ app/src/main/java/com/example/travelbuddy/Main/QRscanactivity.java
@@ -13,8 +13,10 @@ import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 import com.example.travelbuddy.R;
 import com.example.travelbuddy.Reposity.GetDataFromDb;
@@ -33,10 +35,44 @@ public class QRscanactivity extends AppCompatActivity implements ZXingScannerVie
     private String qrCode;
     private SharedViewModel shared;
 
+    ImageButton switchOff,switchOn;
+    Camera camera;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.qractivity);
+
+        switchOff = findViewById(R.id.switch_off);
+        switchOn = findViewById(R.id.switch_on);
+
+        camera = Camera.open();
+        Camera.Parameters parameters = Camera.open().getParameters();
+
+        switchOff.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                switchOff.setVisibility(View.GONE);
+                switchOn.setVisibility(View.VISIBLE);
+                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                camera.setParameters((parameters));
+                camera.startPreview();
+            }
+        });
+        switchOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchOff.setVisibility(View.VISIBLE);
+                switchOn.setVisibility(View.GONE);
+                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                camera.setParameters((parameters));
+                camera.stopPreview();
+            }
+        });
+
+
+
         scannerView = findViewById(R.id.zxscanner);
         Button manualbtn = findViewById(R.id.typeinbtn);
         shared = new ViewModelProvider(this).get(SharedViewModel.class);
