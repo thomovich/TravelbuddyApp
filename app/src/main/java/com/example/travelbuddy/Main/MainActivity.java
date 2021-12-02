65a8016755c0f8c885ffe3b8eec819c3b5f7b4a6
Thomas Petersen <flaskesvar@gmail.com>
2021-12-02 11:57:08 +0100

    QoL improvements to code

--- app/src/main/java/com/example/travelbuddy/Main/MainActivity.java
+++ app/src/main/java/com/example/travelbuddy/Main/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.lifecycle.ViewModelProvider;
+
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -13,11 +15,13 @@ import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
 
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 
 import com.example.travelbuddy.R;
+import com.example.travelbuddy.ViewModels.SharedViewModel;
 import com.google.zxing.Result;
 
 
@@ -27,12 +31,14 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
 public class MainActivity extends AppCompatActivity{
 
     Button scanbutton;
+    SharedViewModel sharedViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         scanbutton = findViewById(R.id.scanbtn);
+        sharedViewModel = new ViewModelProvider(this).get(SharedViewModel.class);
 
         scanbutton.setOnClickListener(view ->{
             Intent intent = new Intent(MainActivity.this,QRscanactivity.class);
@@ -40,6 +46,14 @@ public class MainActivity extends AppCompatActivity{
         });
 
 
+    }
+
+    void checkifscannedqr(){
+        if(sharedViewModel.getQrscanned()){
+            scanbutton.setVisibility(View.INVISIBLE);
+        } else {
+            // do something else
+        }
     }
 
 
