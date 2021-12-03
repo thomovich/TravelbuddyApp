9f63560c504e42ef3e77e2275dfc21062678ed8a
LordofDucks123 <freumert@gmail.com>
2021-12-03 11:57:50 +0100

    Nav bot

--- app/src/main/java/com/example/travelbuddy/Main/MainActivity.java
+++ app/src/main/java/com/example/travelbuddy/Main/MainActivity.java
@@ -6,6 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.Manifest;
 import android.content.DialogInterface;
@@ -22,9 +26,10 @@ import android.widget.Toast;
 
 import com.example.travelbuddy.R;
 import com.example.travelbuddy.ViewModels.SharedViewModel;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.zxing.Result;
 
-
+// import com.example.travelbuddy.databinding.ActivityMainBinding;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
@@ -32,21 +37,42 @@ public class MainActivity extends AppCompatActivity{
 
     Button scanbutton;
     SharedViewModel sharedViewModel;
+   // private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         scanbutton = findViewById(R.id.scanbtn);
         sharedViewModel = new ViewModelProvider(this).get(SharedViewModel.class);
+
+        scanbutton.setOnClickListener(view -> {
+            Intent intent = new Intent (MainActivity.this,QRscanactivity.class);
+            startActivity(intent);
+        });
+
+      /*  binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+       */
+
+       /* BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.fragment_container_view);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
 
         scanbutton.setOnClickListener(view ->{
             Intent intent = new Intent(MainActivity.this,QRscanactivity.class);
             startActivity(intent);
         });
-
+        */
 
     }
+
 
     void checkifscannedqr(){
         if(sharedViewModel.getQrscanned()){
