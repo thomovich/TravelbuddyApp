46346bc8596c978c3bd61306074fe453d57468b5
rasmuskristens <62999973+rasmuskristens@users.noreply.github.com>
2021-12-03 13:33:32 +0100

    GPS map

--- app/src/main/java/com/example/travelbuddy/View/MapFragment.java
+++ app/src/main/java/com/example/travelbuddy/View/MapFragment.java
@@ -1,7 +1,13 @@
 package com.example.travelbuddy.View;
 
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProvider;
+import android.annotation.SuppressLint;
 
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,13 +17,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.travelbuddy.ViewModels.MapViewModel;
 import com.example.travelbuddy.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Marker;
 
-public class MapFragment extends Fragment {
+import java.util.ArrayList;
+
+public class MapFragment extends Fragment implements GoogleMap.OnMarkerClickListener, GoogleMap.OnMyLocationButtonClickListener,OnMapReadyCallback{
 
     private MapViewModel mViewModel;
+
+    private GoogleMap googleMap;
+    private MapViewModel model;
+    MapView mMapView;
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
 
     public static MapFragment newInstance() {
         return new MapFragment();
@@ -26,14 +46,59 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        SupportMapFragment mapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.fragment_container_view);
+        mapFragment.getMapAsync(this);
+
+
         return inflater.inflate(R.layout.map_fragment, container, false);
     }
+
+
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(MapViewModel.class);
+        mViewModel = new ViewModelProvider(getActivity()).get(MapViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    @Override
+    public boolean onMarkerClick(@NonNull Marker marker) {
+        return false;
+    }
+
+
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(getActivity(), "MyLocation button clicked", Toast.LENGTH_SHORT)
+                .show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+            /*googleMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+                @Override
+                public boolean onMyLocationButtonClick() {
+                    googleMap.setMyLocationEnabled(true);
+                    return true;
+                }
+            });
+            googleMap.setOnMyLocationClickListener((GoogleMap.OnMyLocationClickListener) getActivity());*/
+            googleMap.setMyLocationEnabled(true);
+        }
     }
 
 }
