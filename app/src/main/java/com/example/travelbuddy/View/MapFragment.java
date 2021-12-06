32abe55cb36320d2cbe7748769f82c1a3be4b7ce
Thomas Petersen <flaskesvar@gmail.com>
2021-12-06 13:41:02 +0100

    changes

--- app/src/main/java/com/example/travelbuddy/View/MapFragment.java
+++ app/src/main/java/com/example/travelbuddy/View/MapFragment.java
@@ -1,102 +1,83 @@
 package com.example.travelbuddy.View;
-
-
-
-import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
-
-import android.net.Uri;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
-
-import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.travelbuddy.R;
-import com.example.travelbuddy.ViewModels.MapViewModel;
 
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
-import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.single.PermissionListener;
-import com.google.android.gms.location.FusedLocationProviderClient;
-
-import java.util.Map;
 
 
-public class MapFragment extends Fragment implements OnMapReadyCallback{
 
-    boolean ispermissiongranted;
-    MapView mapView;
+public class MapFragment extends Fragment{
 
-    @Nullable
+    private GoogleMap googleMap;
+    MapView mMapView;
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.map_fragment,container,false);
-        mapView = view.findViewById(R.id.mapview);
-        checkMypermission();
-        if(ispermissiongranted){
-            mapView.getMapAsync(this);
-            mapView.onCreate(savedInstanceState);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mMapView.onResume();
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        return view;
-    }
-
-    private void checkMypermission() {
-        Dexter.withContext(getContext()).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
-            @Override
-            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                ispermissiongranted = true;
-            }
+        mMapView.getMapAsync(new OnMapReadyCallback() {
 
             @Override
-            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
-                Intent intent = new Intent();
-                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                Uri uri = Uri.fromParts("package",getContext().getPackageName(),"");
-                intent.setData(uri);
-                startActivity(intent);
-            }
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+                if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED  ){
+                    requestPermissions(new String[]{
+                                    android.Manifest.permission.ACCESS_FINE_LOCATION},
+                            REQUEST_CODE_ASK_PERMISSIONS);
+                    return ;
+                }
 
-            @Override
-            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
-                permissionToken.continuePermissionRequest();
+                googleMap.setMyLocationEnabled(true);
+
+
             }
         });
+
+        super.onViewCreated(view, savedInstanceState);
     }
+
+
 
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.map_fragment, container, false);
+        mMapView = rootView.findViewById(R.id.mapview);
+        mMapView.onCreate(savedInstanceState);
 
+
+
+
+
+
+
+
+
+        return rootView;
     }
 
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-    }
+
+
+
     /*@Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         checkPermission();
