750c6d1a0e950b8cd879fa881517075bf97a6bb4
Thomas Petersen <flaskesvar@gmail.com>
2021-12-13 13:57:22 +0100

    maps

--- app/src/main/java/com/example/travelbuddy/Reposity/dblookups.java
+++ app/src/main/java/com/example/travelbuddy/Reposity/dblookups.java
@@ -2,13 +2,14 @@ package com.example.travelbuddy.Reposity;
 
 import android.media.MediaPlayer;
 
+import com.example.travelbuddy.Models.Sight;
 import com.example.travelbuddy.Models.Sights;
 
 import java.util.ArrayList;
 
 public interface dblookups {
     boolean checkqr (String Qrcode);
-    ArrayList<String> getcoord(String Qrcode);
+    ArrayList<Sight> getcoord(String Qrcode);
     MediaPlayer getsound(String Qrcode);
     ArrayList<Sights> getSights(String Qrcode);
 }
