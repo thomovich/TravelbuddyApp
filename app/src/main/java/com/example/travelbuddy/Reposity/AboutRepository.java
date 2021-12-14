fe9a6c49b6d84ed95b0200bda37c7932900cba0d
LordofDucks123 <freumert@gmail.com>
2021-12-14 12:36:26 +0100

    pickle

--- app/src/main/java/com/example/travelbuddy/Reposity/AboutRepository.java
+++ app/src/main/java/com/example/travelbuddy/Reposity/AboutRepository.java
@@ -19,7 +19,7 @@ public class AboutRepository {
         aboutList.add(new About("Dette er Asbjørn. Han er 24 år gammel og er vores Lead-Designer", R.drawable.asbjorn));
         aboutList.add(new About("Dette er Frederik. Han er 26 år gammel og arbejder som CEO", R.drawable.frederik));
         aboutList.add(new About("Dette er Simon. Han er 22 år gammel or arbejder som CEO", R.drawable.simon));
-        aboutList.add(new About("Dette er rasmus. Han er 25 år gammel og arbejder som vicevært", R.drawable.rasmus));
+        aboutList.add(new About("Dette er rasmus. Han er 25 år gammel og arbejder som vicevært", R.drawable.pickle));
         aboutList.add(new About("Dette er Thomas. Han er 25 år gammel og arbejder som HR-medarbejder", R.drawable.thomas));
         aboutList.add(new About("Virksomheden er 6 måneder gammel. Vores mål er at lave en applikation der kan hjælpe folk med at oplever byer og kulturer rundt omkring i verden, imens de bliver aktiveret både fysisk og mentalt",R.drawable.agnete));
 
@@ -29,7 +29,7 @@ public class AboutRepository {
         aboutList.add(new About("This is Asbjørn. He is 24 years old and is our lead designer", R.drawable.asbjorn));
         aboutList.add(new About("This is Frederik. He is 26 years old and work in the company as the CE", R.drawable.frederik));
         aboutList.add(new About("This is Simon. He is 22 years old, and work as CEO", R.drawable.simon));
-        aboutList.add(new About("This is rasmus. He is 25 years old. He is in charge of all pickles.", R.drawable.rasmus));
+        aboutList.add(new About("This is rasmus. He is 25 years old. He is in charge of all pickles.", R.drawable.pickle));
         aboutList.add(new About("This is Thomas. He is head og the janitors in the company", R.drawable.thomas));
         aboutList.add(new About("The company is 6 months old and our goal is to make an application that can help people experience great citites and wonders around the world while being active physicaly and mentally",R.drawable.agnete));
 
